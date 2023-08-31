c49e8855dcd54ef0e817b7a48dcf992df4648211
billal <boudjellbillal@gmail.com>
2023-08-31 13:19:04 +0200

    logout

--- src/redux/apiCalls/authApiCalls.jsx
+++ src/redux/apiCalls/authApiCalls.jsx
@@ -3,36 +3,44 @@ import { authActions } from "../slice/authSlice"
 import { toast } from "react-toastify"
 
 
-export function SingUpUser(user){
-    return async (dispatch,getState)=>{
-         try {
+export function SingUpUser(user) {
+    return async (dispatch, getState) => {
+        try {
             dispatch(authActions.setLoading())
-            const {data} = await axios.post('https://ecommerce-ypmm.onrender.com/api/v1/auth/signup',user)
+            const { data } = await axios.post('https://ecommerce-ypmm.onrender.com/api/v1/auth/signup', user)
             alert(data.message)
             console.log(data);
             dispatch(authActions.clearLoading())
             dispatch(authActions.register(data))
-            localStorage.setItem('userEcm',JSON.stringify(data))
-         } catch (error) {
+            localStorage.setItem('userEcm', JSON.stringify(data))
+        } catch (error) {
             alert(error.response.data.errors[0].msg)
             dispatch(authActions.clearLoading())
-         }
+        }
     }
 }
 
 
-export function signinUser(user){
-    return async (dispatch,getState)=>{
-         try {
+export function signinUser(user) {
+    return async (dispatch, getState) => {
+        try {
             dispatch(authActions.setLoading())
-            const {data} = await axios.post('https://ecommerce-ypmm.onrender.com/api/v1/auth/login',user)
+            const { data } = await axios.post('https://ecommerce-ypmm.onrender.com/api/v1/auth/login', user)
             alert("logged in successfully")
             dispatch(authActions.clearLoading())
             dispatch(authActions.login(data))
-            localStorage.setItem('userEcm',JSON.stringify(data))
-         } catch (error) {
+            localStorage.setItem('userEcm', JSON.stringify(data))
+        } catch (error) {
             alert(error.response.data.errors[0].msg)
             dispatch(authActions.clearLoading())
-         }
+        }
+    }
+}
+
+//logout user
+export function logoutUser() {
+    return async (dispatch, getState) => {
+        dispatch(authActions.logout())
+        localStorage.removeItem("userEcm")
     }
 }
